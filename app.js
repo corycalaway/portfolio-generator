6a3db1b2b0134707a645409846bf889289b75901
corycalaway <cory.c.calaway@gmail.com>
2021-01-12 17:49:11 -0700

    intalled npm, added gitignore, added prompt with question and return

--- app.js
+++ app.js
@@ -1,39 +1,21 @@
-const generatePage = require('./src/page-template.js');
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
 
-const fs = require('fs');
+// const pageHTML = generatePage(name, github);
 
-const profileDataArgs = process.argv.slice(2);
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
 
-const [name, github] = profileDataArgs;
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
 
-
-
-fs.writeFile('./index.html', generatePage(name, github), err => {
-  if (err) throw new Error(err);
-
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
-
-// const http = require ('http');
-// const name = profileDataArgs[0];
-// const github = profileDataArgs[1];
-// console.log(profileDataArgs);
-
-// const printProfileData = profileDataArr => {
-//     // This...
-//     for (let i = 0; i < profileDataArr.length; i += 1) {
-//       console.log(profileDataArr[i]);
-//     }
-  
-//     console.log('================');
-  
-//     // Is the same as this...
-//     profileDataArr.forEach(profileItem => console.log(profileItem));
-//   };
-  
-// printProfileData(profileDataArgs);
-
-// const generatePage = (userName, githubName) => `Name: ${userName}, Github: ${githubName}`;
-
-
-
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
